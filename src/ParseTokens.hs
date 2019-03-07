2fc9651c306b78ca7474226cea8fcf4a49722416
Lucie Philippon (Julien) <julien.philippon@epitech.eu>
2019-03-07 18:12:36 +0100

    Add function prototypes to ParseTokens.hs

--- src/ParseTokens.hs
+++ src/ParseTokens.hs
@@ -3,30 +3,35 @@ module ParseTokens (parseTokens) where
 instructions = ["def","for","while"]
 binop = ["==","!=","=","+","*","-","/","<",">"]
 unop = ["!","-"]
-blockDelimiters = [":","(",")",";"]
+blockDelimiters = [":","(",")",";",","]
 types = ["int","double","void"]
 allTokens = binop ++ unop ++ blockDelimiters
 
+removeBlanks :: String -> String
 removeBlanks str = dropWhile (`elem` " \t\n") str
 
+isStartOfString :: String -> String -> Bool
 isStartOfString start str =
   let len = (length start) in
     (take len str) == start
 
+getToken :: String -> [String] -> String
 getToken str []
   | (getIdentifier str) /= [] = (getIdentifier str)
-  | otherwise = []
+  | otherwise = errorWithoutStackTrace ("Unrecognized token: " ++ (take 1 str))
 getToken str tokens =
   if (isStartOfString (head tokens) str)
   then (head tokens)
   else (getToken str (tail tokens))
 
+getIdentifier :: String -> String
 getIdentifier [] = []
 getIdentifier str =
   if (elem (head str) (['A'..'Z'] ++ ['a'..'z'] ++ ['0'..'9'] ++ "."))
   then (head str) : (getIdentifier (tail str))
   else []
 
+parseTokens :: String -> [String]
 parseTokens [] = []
 parseTokens file =
   let token = (getToken file allTokens) in
